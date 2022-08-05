b638eb02b3945584a840d9212846268239c38cc4
dheepsk <96794949+dheepsk@users.noreply.github.com>
2022-08-05 11:21:05 -0500

    Create countries.py

---
+++ countries.py
@@ -0,0 +1,12 @@
+countries = {
+  'Canada': 1.29, #canadian dollar
+  # 'East Asia': 134.22, #japanese yen
+  'General Europe': 0.99, #euro
+  # 'United Kingdom': 0.83, #pound sterling
+  # 'India': 79.15, #indian rupee
+  # 'Africa': 16.86, ##south african rand
+  # 'South America': 20.64, #mexican peso
+  'Australia': 1.44 #australiian dollar
+}
+
+
